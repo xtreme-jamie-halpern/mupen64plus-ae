628e3484f0d5992526faf53306abf84102ba9826
littleguy77 <mattlichter@live.com>
2013-03-14 11:06:02 -0400

    gles2rice: Fixed mipmapping/anisotropic filt, integrated upstream code.
    OGLTexture.cpp fully merged with upstream.

--- jni/gles2rice/src/OGLTexture.cpp
+++ jni/gles2rice/src/OGLTexture.cpp
@@ -109,20 +109,23 @@ void COGLTexture::EndUpdate(DrawInfo *di)
         int m_maximumAnistropy = pcontext->getMaxAnisotropicFiltering(); //if getMaxAnisotropicFiltering() return more than 0, so aniso is supported and maxAnisotropicFiltering is set
 
         // Set Anisotropic filtering (mipmapping have to be activated, aniso filtering is not effective without)
-//        if( m_maximumAnistropy )
-//        {
-//            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_ANISOTROPY_EXT, m_maximumAnistropy);
-//            OPENGL_CHECK_ERRORS;
-//        }
+        if( m_maximumAnistropy )
+        {
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_ANISOTROPY_EXT, m_maximumAnistropy);
+            OPENGL_CHECK_ERRORS;
+        }
 
         // Set Mipmap
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST);
         OPENGL_CHECK_ERRORS;
 
         // Tell to hardware to generate mipmap (himself) when glTexImage2D is called
-        //TODO:Check Mipmapping
-        //glTexParameteri(GL_TEXTURE_2D, GL_GENERATE_MIPMAP, GL_TRUE);
-        //OPENGL_CHECK_ERRORS;
+#if SDL_VIDEO_OPENGL
+        glTexParameteri(GL_TEXTURE_2D, GL_GENERATE_MIPMAP, GL_TRUE);
+#elif SDL_VIDEO_OPENGL_ES2
+        glGenerateMipmap(GL_TEXTURE_2D);
+#endif
+        OPENGL_CHECK_ERRORS;
     }
     else
     {
@@ -130,9 +133,12 @@ void COGLTexture::EndUpdate(DrawInfo *di)
         OPENGL_CHECK_ERRORS;
     }
 
-    // GL_BGRA_IMG works on adreno but not inside profiler.
     // Copy the image data from main memory to video card texture memory
+#if SDL_VIDEO_OPENGL
+    glTexImage2D(GL_TEXTURE_2D, 0, m_glFmt, m_dwCreatedTextureWidth, m_dwCreatedTextureHeight, 0, GL_BGRA_EXT, GL_UNSIGNED_BYTE, m_pTexture);
+#elif SDL_VIDEO_OPENGL_ES2
     glTexImage2D(GL_TEXTURE_2D, 0, m_glFmt, m_dwCreatedTextureWidth, m_dwCreatedTextureHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, m_pTexture);
+#endif
     OPENGL_CHECK_ERRORS;
 }
 
