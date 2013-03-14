e21578e90ff8ce260d42a17978f13e059a6c7e68
Paul Lamb <paul@paulscode.com>
2013-03-14 19:18:50 -0400

    OUYA: Remove controller diagnostics section

--- src/paulscode/android/mupen64plusae/MenuActivity.java
+++ src/paulscode/android/mupen64plusae/MenuActivity.java
@@ -111,6 +111,7 @@ public class MenuActivity extends PreferenceActivity implements OnPreferenceClic
         {
             new Builder( this ).setTitle( "OUYA Hardware Not Detected" ).setMessage( "isRunningOnOUYAHardware returned false!  Do you have the latest system update installed?  Please restart your OUYA and select the option to check for system updates" ).create().show();
         }
+        PrefUtil.removePreference( this, "screenAdvanced", "categoryDiagnostics" );
         
         // Disable the Xperia PLAY plugin as necessary
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences( this );
